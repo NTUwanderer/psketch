0d3a26e4c640aafef8e36743f527cf4e270cec67
NTUwanderer <harveyycyang@gmail.com>
2018-05-17 00:45:09 +0800

    Beta learner

--- trainers/learner.py
+++ trainers/learner.py
@@ -11,7 +11,7 @@ from .dataset import Dataset
 
 class Learner:
     # TODO
-    def __init__(self, policy, old_policy, num_subpolicies, comm, clip_param=0.2, entcoeff=0, optim_epochs=10, optim_stepsize=3e-4, optim_batchsize=64):
+    def __init__(self, policy, old_policy, env_model, old_env_model, num_subpolicies, comm, clip_param=0.2, entcoeff=0, optim_epochs=10, optim_stepsize=3e-4, optim_batchsize=64):
         self.policy = policy
         self.clip_param = clip_param
         self.entcoeff = entcoeff
@@ -27,9 +27,11 @@ class Learner:
         # for training theta
         # inputs for training theta
         ob = U.get_placeholder_cached(name="ob")
+        new_ob = U.get_placeholder_cached(name="new_ob")
         ac = policy.pdtype.sample_placeholder([None])
         atarg = tf.placeholder(dtype=tf.float32, shape=[None]) # Target advantage function (if applicable)
         ret = tf.placeholder(dtype=tf.float32, shape=[None]) # Empirical return
+
         total_loss = self.policy_loss(policy, old_policy, ob, ac, atarg, ret, clip_param)
         self.master_policy_var_list = policy.get_trainable_variables()
         self.master_loss = U.function([ob, ac, atarg, ret], U.flatgrad(total_loss, self.master_policy_var_list))
@@ -38,34 +40,14 @@ class Learner:
         self.assign_old_eq_new = U.function([],[], updates=[tf.assign(oldv, newv)
             for (oldv, newv) in zipsame(old_policy.get_variables(), policy.get_variables())])
 
-        # self.assign_subs = []
-        # self.change_subs = []
-        # self.adams = []
-        # self.losses = []
-        # self.sp_ac = sub_policies[0].pdtype.sample_placeholder([None])
-        # for i in range(self.num_subpolicies):
-        #     varlist = sub_policies[i].get_trainable_variables()
-        #     self.adams.append(MpiAdam(varlist))
-        #     loss for test
-        #     loss = self.policy_loss(sub_policies[i], old_sub_policies[i], ob, self.sp_ac, atarg, ret, clip_param)
-        #     self.losses.append(U.function([ob, self.sp_ac, atarg, ret], U.flatgrad(loss, varlist)))
-
-        #     self.assign_subs.append(U.function([],[], updates=[tf.assign(oldv, newv)
-        #         for (oldv, newv) in zipsame(old_sub_policies[i].get_variables(), sub_policies[i].get_variables())]))
-        #     self.zerograd = U.function([], self.nograd(varlist))
+        env_model_loss = self.env_model_loss(env_model, old_env_model, new_ob, ac, ret, clip_param)
+        self.env_model_var_list = env_model.get_trainable_variables()
+        self.env_model_loss = U.function([new_ob, ac, ret], U.flatgrad(env_model_loss, self.env_model_var_list))
+        self.env_model_adam = MpiAdam(self.env_model_var_list, comm=comm)
 
         U.initialize()
 
-        self.master_adam.sync()
-        # for i in range(self.num_subpolicies):
-        #     self.adams[i].sync()
-
-    # def nograd(self, var_list):
-    #     return tf.concat(axis=0, values=[
-    #         tf.reshape(tf.zeros_like(v), [U.numel(v)])
-    #         for v in var_list
-    #     ])
-
+        self.syncMasterPolicies()
 
     def policy_loss(self, pi, oldpi, ob, ac, atarg, ret, clip_param):
         ratio = tf.exp(pi.pd.logp(ac) - tf.clip_by_value(oldpi.pd.logp(ac), -20, 20)) # advantage * pnew / pold
@@ -79,12 +61,22 @@ class Learner:
         total_loss = pol_surr + vf_loss
         return total_loss
 
+    def env_model_loss(self, m, old_m, new_ob, ac, ret, clip_param):
+        vfloss1 = tf.square(m.vpred - ret)
+        vpredclipped = m.vpred + tf.clip_by_value(m.vpred - old_m.vpred, -clip_param, clip_param)
+        vfloss2 = tf.square(vpredclipped - ret)
+        vf_loss = .5 * U.mean(tf.maximum(vfloss1, vfloss2))
+
+        env_loss = tf.reduce_sum(tf.square(m.env_pred - new_ob))
+
+        env_model_loss = vf_loss + env_loss
+        return env_model_loss
+
     def syncMasterPolicies(self):
         self.master_adam.sync()
 
-    # def syncSubpolicies(self):
-    #     for i in range(self.num_subpolicies):
-    #         self.adams[i].sync()
+    def syncEnvModel(self):
+        self.env_model_adam.sync()
 
     def updateMasterPolicy(self, ep_lens, ep_rets, ob, ac, atarg, tdlamret):
         #ob, ac, atarg, tdlamret = seg["macro_ob"], seg["macro_ac"], seg["macro_adv"], seg["macro_tdlamret"]
@@ -118,6 +110,39 @@ class Learner:
         logger.record_tabular("EpRewMean", np.mean(rews))
 
         return np.mean(rews), np.mean(ep_rets)
+
+    def updateEnvModel(self, ep_lens, ep_rets, ob, ac):
+#         #ob, ac, atarg, tdlamret = seg["macro_ob"], seg["macro_ac"], seg["macro_adv"], seg["macro_tdlamret"]
+#         # ob = np.ones_like(ob)
+#         mean = atarg.mean()
+#         std = atarg.std()
+#         meanlist = MPI.COMM_WORLD.allgather(mean)
+#         global_mean = np.mean(meanlist)
+# 
+#         real_var = std**2 + (mean - global_mean)**2
+#         variance_list = MPI.COMM_WORLD.allgather(real_var)
+#         global_std = np.sqrt(np.mean(variance_list))
+# 
+#         atarg = (atarg - global_mean) / max(global_std, 0.000001)
+# 
+#         d = Dataset(dict(ob=ob, ac=ac, atarg=atarg, vtarg=tdlamret), shuffle=True)
+#         optim_batchsize = min(self.optim_batchsize,ob.shape[0])
+# 
+#         self.policy.ob_rms.update(ob) # update running mean/std for policy
+# 
+#         self.assign_old_eq_new()
+#         for _ in range(self.optim_epochs):
+#             for batch in d.iterate_once(optim_batchsize):
+#                 g = self.master_loss(batch["ob"], batch["ac"], batch["atarg"], batch["vtarg"])
+#                 self.master_adam.update(g, 0.01, 1)
+# 
+#         # lrlocal = (seg["ep_lens"], seg["ep_rets"]) # local values
+#         lrlocal = (ep_lens, ep_rets)
+#         listoflrpairs = MPI.COMM_WORLD.allgather(lrlocal) # list of tuples
+#         lens, rews = map(flatten_lists, zip(*listoflrpairs))
+#         logger.record_tabular("EpRewMean", np.mean(rews))
+# 
+#         return np.mean(rews), np.mean(ep_rets)
 
 def flatten_lists(listoflists):
     return [el for list_ in listoflists for el in list_]
